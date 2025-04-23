be0779621a73e8e0cc968ce537ac56c6fc54130f
aahmadf123 <firas.azfar@gmail.com>
2025-04-23 13:12:31 -0400

    import statement update

--- dags/tasks/__init__.py
+++ dags/tasks/__init__.py
@@ -25,7 +25,8 @@ if dags_dir not in sys.path:
 from .ingestion import ingest_data_from_s3
 from .preprocessing import preprocess_data
 from .schema_validation import validate_schema, snapshot_schema
-from .training import train_and_compare_fn, ModelExplainabilityTracker
+from .training import train_and_compare_fn
+from .model_explainability import ModelExplainabilityTracker
 from .ab_testing import ABTestingPipeline
 from .monitoring import DataQualityMonitor, generate_reference_means, detect_data_drift, self_healing, record_system_metrics, update_monitoring_with_ui_components
 from .data_quality import manual_override
